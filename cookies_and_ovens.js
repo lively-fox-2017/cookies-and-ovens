5d85a8b33ddab3fe976e5b89919b7681339e0580
Saptanto Sindu <saptanto.sindu@gmail.com>
2017-09-26 14:28:32 +0700

    Commit Release 1

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,71 @@
 //
 //
 // Your code here
+class Cookie {
+  constructor(name) {
+    this.name = name;
+    this.cookingTime = 0;
+    this.status = null;
+  }
+
+  bakedProgress(time) {
+    if (time >= 0 && time < this.cookingTime / 2)
+      this.status = 'Mentah';
+    else if (time >= this.cookingTime / 2 && time < this.cookingTime)
+      this.status = 'Hampir Matang';
+    else if (time === this.cookingTime)
+      this.status = 'Matang';
+    else
+      this.status = 'Hangus';
+    console.log('Kue ' + this.name + ', menit ke ' + time + ' : ' + this.status);
+  }
+}
+
+class ChocolateCookie extends Cookie {
+  constructor(name) {
+    super(name);
+    this.cookingTime = 20;
+  }
+}
+
+class PeanutCookie extends Cookie {
+  constructor(name) {
+    super(name);
+    this.cookingTime = 30;
+  }
+}
+
+class CheeseCookie extends Cookie {
+  constructor(name) {
+    super(name);
+    this.cookingTime = 35;
+  }
+}
+
+class Chef {
+  static bake(options) {
+    if (options.hasOwnProperty('name') && options.hasOwnProperty('time')) {
+      let cookie = null;
+      switch (options['name']) {
+        case 'coklat':
+          cookie = new ChocolateCookie(options['name']);
+          break;
+        case 'kacang':
+          cookie = new PeanutCookie(options['name']);
+          break;
+        case 'keju':
+          cookie = new CheeseCookie(options['name']);
+          break;
+        default:
+          return -1;
+          break;
+      }
+
+      for (let i = 0; i <= options['time']; i = i + 5) {
+        cookie.bakedProgress(i);
+      }
+    }
+  }
+}
+
+Chef.bake({name: 'coklat', time: 30});
